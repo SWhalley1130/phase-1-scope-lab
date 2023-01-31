ca561e9a7f9258b7d015a040fef5f6e56c35bcc6
Sarah Whalley <s.whalley1130@gmail.com>
2023-01-31 10:33:37 -0700

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName ="bob";
+
+function upperCaseCustomerName()
+{
+    return customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer()
+{
+    bestCustomer="not bob";
+}
+
+function overwriteBestCustomer()
+{
+    return bestCustomer='maybe bob';
+}
+
+const leastFavoriteCustomer=`bob's wife`;
+
+function changeLeastFavoriteCustomer()
+{
+    return leastFavoriteCustomer='brenda';
+}
